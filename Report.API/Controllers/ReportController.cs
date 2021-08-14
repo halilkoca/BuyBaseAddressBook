78fa78b8afd8c4b6b93d6ac1afb6cd3050dc19a0
Halil Koca <ihalilkoca@gmail.com>
2021-08-14 11:49:35 +0300

    added Report.API project

---
+++ Report.API/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Report.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        private readonly ILogger<ReportController> _logger;
+
+        public ReportController(ILogger<ReportController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
