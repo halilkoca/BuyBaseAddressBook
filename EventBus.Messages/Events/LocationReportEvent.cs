87d2ef8ac668bb442f94cb3807edfc37be08e27f
Halil Koca <ihalilkoca@gmail.com>
2021-08-15 12:06:25 +0300

    added eventbus.messages class library

---
+++ EventBus.Messages/Events/LocationReportEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class LocationReportEvent
+    {
+        public string Location { get; set; }
+        public int PeopleCount { get; set; }
+        public int PhoneNumberCount { get; set; }
+    }
+}
